6a7176a11e37e921ba2eb6f5b3679cf9c529dd0a
Jeyabalaji <jeyabalaji.tm@grabtaxi.com>
2023-10-13 11:18:09 +0530

    added exception in pro gurar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,11 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
--- app/src/main/java/com/antandbuffalo/quicknote/service/ApiService.java
+++ app/src/main/java/com/antandbuffalo/quicknote/service/ApiService.java
@@ -1,12 +1,8 @@
 package com.antandbuffalo.quicknote.service;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 
 public interface ApiService {
 //    https://sample-node-api-antandbuffalo.vercel.app/
